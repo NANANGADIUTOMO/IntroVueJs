ac76447a435c7707ab54459a8fa99d1a42d0c5af
NANANG ADI-UTOMO <utomo17121999@gmail.com>
2022-11-23 08:43:36 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{nama}}</h1>
+    <h2>ini anggota saya {{ anggota[0]}}</h2>
+
+    <button 
+    v-on:click="gantinama">Ganti nama 
+    </button>
+
+    <h2>{{totalBelanja}}</h2>
+    <button 
+    v-on:click="gantiquantity">Ganti Qty 
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data : function(){
+    return{
+      nama : "Paguyuban Magicom Sakti",
+      anggota : ["bagas", "alif", "ayom"],
+      quantity: 5
+    }
+  },
+  methods:{
+    gantinama : function(){
+      if(this.nama == "Paguyuban Magicom Sakti"){
+          this.nama = "paguyuban centong Kebak"
+      }else{
+        this.nama = "Paguyuban Magicom Sakti"
+      }
+    },
+    gantiquantity : function(){
+      this.quantity++
+    }
+  },
+  computed:{
+    totalBelanja :   function(){
+      return this.quantity * 10000
+    }
+  },
   components: {
-    HelloWorld
+    //HelloWorld
   }
 }
 </script>
